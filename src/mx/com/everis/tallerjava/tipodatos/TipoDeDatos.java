9121d9eba43197bce807aeaeb593737ef918544e
fritz <fritz@HorrorPC>
2016-11-14 19:09:05 -0600

    ...

---
+++ src/mx/com/everis/tallerjava/tipodatos/TipoDeDatos.java
@@ -0,0 +1,10 @@
+package mx.com.everis.tallerjava.tipodatos;
+
+public class TipoDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("....");
+	}
+
+}
